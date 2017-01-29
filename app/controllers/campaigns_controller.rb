77e767ed8f4c2d7c7fbba17ea11acc99326ba292
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-01-29 20:04:33 +0100

    added message action

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,5 @@
 class CampaignsController < ApplicationController
+  before_action :set_campaign, only: [:edit, :update, :message, :preview, :schedule, :destroy]
   def index
   end
 
@@ -7,7 +8,22 @@ class CampaignsController < ApplicationController
     @mailing_lists = MailingList.all
   end
 
+  def message
+  end
+
+  def preview
+  end
+
+  def schedule
+  end
+
   def create
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      redirect_to :action => "message", :id => @campaign.id
+    else
+      render :new
+    end
   end
 
   def edit
@@ -17,5 +33,18 @@ class CampaignsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_campaign
+    @campaign = Campaign.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def campaign_params
+    params.require(:campaign).permit(
+      :name
+      )
   end
 end
