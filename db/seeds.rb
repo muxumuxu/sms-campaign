bbf730ed1255697a9151eb5d961a997be58f2c5a
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-02-07 13:33:15 +0100

    added table of contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,35 @@ picardie = MailingList.new(
   name: "Picardie"
 )
 picardie.save
+
+# Contacts #
+
+audrey = Contact.new(
+  first_name: "Audrey",
+  last_name: "Présidente",
+  phone_number: "0987654321",
+  zip_code: "33000"
+)
+audrey.save
+
+luc = Contact.new(
+  first_name: "Luc",
+  last_name: "Chaissac",
+  phone_number: "0987654322",
+  zip_code: "33000"
+)
+luc.save
+
+# Contacts Mailing Lists #
+
+audrey_gironde = ContactsMailingList.new(
+  contact_id: 1,
+  mailing_list_id: 1
+)
+audrey_gironde.save
+
+luc_gironde = ContactsMailingList.new(
+  contact_id: 2,
+  mailing_list_id: 1
+)
+luc_gironde.save
