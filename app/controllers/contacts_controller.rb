e9083c33501ed84521fef1e0b5e1da088b568636
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-02-09 14:50:04 +0100

    new contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,18 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: [:edit, :update, :destroy]
+  before_action :set_mailing_list, only: [:create]
   def new
     @contact = Contact.new
-    @contact.mailing_lists = [MailingList.find(params[:mailing_list_id])]
+    @contact.mailing_lists = [ MailingList.find(params[:mailing_list_id]) ]
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to @mailing_list
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +28,19 @@ class ContactsController < ApplicationController
 
   def set_contact
     @contact = Contact.find(params[:id])
+  end
+
+  def set_mailing_list
+    @mailing_list = MailingList.find(params[:mailing_list_id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(
+      :phone_number,
+      :first_name,
+      :last_name,
+      :zip_code,
+      :mailing_list_id
+      )
   end
 end
