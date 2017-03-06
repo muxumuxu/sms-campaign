9aad87c610960b65d1cd85166bf9e59e0b0d53bd
David Miotti <david@muxumuxu.com>
2017-03-06 23:14:33 +0100

    add csv import

--- app/controllers/mailing_lists_controller.rb
+++ app/controllers/mailing_lists_controller.rb
@@ -1,5 +1,8 @@
 class MailingListsController < ApplicationController
-  before_action :set_mailing_list, only: [:edit, :update, :show, :upload_csv, :import_csv, :import_csv_results, :destroy]
+  skip_before_filter :verify_authenticity_token
+  before_action :set_mailing_list, only: [
+    :edit, :update, :show, :upload_csv, :import_csv_results, :destroy, :import_csv
+  ]
 
   def index
     @mailing_lists = MailingList.where("user_id = #{current_user.id}")
@@ -19,7 +22,7 @@ class MailingListsController < ApplicationController
     @mailing_list.user = current_user
     if @mailing_list.save
       if params[:mailing_list][:import_type] == "csv"
-        redirect_to :action => :import_csv, :id => @mailing_list.id
+        redirect_to :action => :upload_csv, :id => @mailing_list.id
       else
         redirect_to :action => :index
       end
@@ -44,6 +47,29 @@ class MailingListsController < ApplicationController
   end
 
   def import_csv
+    file_data = upload_file_params[:file]
+    
+    if file_data.respond_to?(:read)
+      csv_contents = file_data.read
+    elsif file_data.respond_to?(:path)
+      csv_contents = File.read(file_data.path)
+    else
+      raise "Bad file_data: #{file_data.class.name}: #{file_data.inspect}"
+    end
+
+    CSV.parse(csv_contents) do |row|
+      if Contact.where(phone_number: row[0], mailing_list_id: @mailing_list.id).count == 0
+        contact = Contact.new
+        contact.phone_number = row[0]
+        contact.first_name = row[1]
+        contact.last_name = row[2]
+        contact.zip_code = row[3]
+        contact.mailing_list_id = @mailing_list.id
+        contact.save!
+      end
+    end
+
+    redirect_to :action => :index
   end
 
   def import_csv_results
@@ -62,5 +88,9 @@ class MailingListsController < ApplicationController
 
   def mailing_list_params
     params.require(:mailing_list).permit(:name, :import_type).except(:import_type)
+  end
+
+  def upload_file_params
+    params.require(:mailing_list).permit(:file)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   resources :campaigns do
     member do
-      get :name, to: "campaigns#name"
-      get :message, to: "campaigns#message"
-      get :preview, to: "campaigns#preview"
-      get :schedule, to: "campaigns#schedule"
+      get :name
+      get :message
+      get :preview
+      get :schedule
     end
   end
   resources :mailing_lists do
     member do
       get :upload_csv
-      get :import_csv
-      get :import_csv_results
-
       post :import_csv
     end
   end
