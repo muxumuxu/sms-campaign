1c1186109eb4476e2efaaf9ea1b09fb0a21f1b36
David Miotti <david@muxumuxu.com>
2017-03-13 02:01:50 +0100

    remove useless comment

--- app/jobs/send_campaign_job.rb
+++ app/jobs/send_campaign_job.rb
@@ -2,12 +2,9 @@ class SendCampaignJob < ApplicationJob
   queue_as :default
 
   def perform(campaign_id)
-
-  	puts "Starting jon for #{campaign_id}"
   	campaign = Campaign.find(campaign_id)
   	return unless campaign.sent_at.nil?
-  	return if campaign.scheduled_at.nil?
-  	
+  	return if campaign.scheduled_at.nil?  	
     campaign.send_campaign()
   end
 end
