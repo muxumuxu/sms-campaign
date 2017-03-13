5da2aa6ce20b10a230d039d3ae58c98f5781907f
David Miotti <david@muxumuxu.com>
2017-03-13 01:27:14 +0100

    add sidekiq deployment in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 docker-compose stop
 docker-compose run web rails assets:precompile
 heroku container:push web --app sms-campaign
+heroku container:push sidekiq --app sms-campaign
 heroku run rails db:migrate --app sms-campaign
