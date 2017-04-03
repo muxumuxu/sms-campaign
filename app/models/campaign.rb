e6eb59d3cb7f4a2ce89778447f1b2ae72911b60e
David Miotti <david.miotti@wopata.com>
2017-04-03 16:21:03 +0200

    Keep sending message if one is failing

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -8,22 +8,24 @@ class Campaign < ApplicationRecord
     mailing_list = self.mailing_list
     contacts = mailing_list.contacts
 
-    begin
-      twilio_number = ENV['TWILIO_NUMBER']
-      client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
-      contacts.each do |contact|
-        message = build_message_for(contact, self[:message])
+    twilio_number = ENV['TWILIO_NUMBER']
+    client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+    contacts.each do |contact|
+      message = build_message_for(contact, self[:message])
+      begin
         message = client.account.messages.create(
-          :from => twilio_number,
-          :to => contact.phone_number,
-          :body => message
+          from: twilio_number,
+          to: contact.phone_number,
+          body: message
         )
+      rescue Exception => e
+        Rollbar.error(e)
       end
-      self[:sent_at] = DateTime.now
-      self.save!
-    rescue Exception => e
-      Rollbar.error(e)
     end
+
+    self[:sent_at] = DateTime.now
+    self.save!
   end
 
   private
